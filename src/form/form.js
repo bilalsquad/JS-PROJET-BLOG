abbd56ed193351dc0c5b333b3162ede463c15a5c
“Barar <[barar.bilal@laposte.net]>
2021-03-01 23:43:14 +0100

    rajout de JS pour l'ajout d'article sur la page d'acceuil

--- src/form/form.js
+++ src/form/form.js
@@ -1,4 +1,4 @@
-import "../assets/styles/style.scss";
+import "../assets/styles/styles.scss";
 import "./form.scss";
 
 const form = document.querySelector("form");
@@ -6,33 +6,34 @@ const errorElement = document.querySelector("#errors");
 let errors = [];
 
 form.addEventListener("submit", async event => {
-    event.preventDefault();
-    const formData = new FormData(form);
-    const article = Object.fromEntries(formData.entries());
-    if (formIsValid(article)) {
-      try {
-        const json = JSON.stringify(article);
-        const response = await fetch("https://restapi.fr/api/article", {
-          method: "POST",
-          body: json,
-          headers: {
-            "Content-Type": "application/json"
-          }
-        });
-        const body = await response.json();
-        console.log(body);
-      } catch (e) {
-        console.error("e : ", e);
-      }
+  event.preventDefault();
+  const formData = new FormData(form);
+  const article = Object.fromEntries(formData.entries());
+  if (formIsValid(article)) {
+    try {
+      const json = JSON.stringify(article);
+      const response = await fetch("https://restapi.fr/api/article", {
+        method: "POST",
+        body: json,
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+      const body = await response.json();
+      console.log(body);
+    } catch (e) {
+      console.error("e : ", e);
     }
-  });
-
+  }
+});
 
 const formIsValid = article => {
   if (
     !article.author ||
     !article.category ||
-    !article.content
+    !article.content ||
+    !article.img ||
+    !article.title
   ) {
     errors.push("Vous devez renseigner tous les champs");
   } else {
